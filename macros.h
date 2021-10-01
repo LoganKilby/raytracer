4e338695f399c3c73e200c1ba03caf0269820b88
Logan Kilby <logankilby@gmail.com>
2021-10-01 17:44:20 -0500

    math

--- macros.h
+++ macros.h
@@ -4,13 +4,17 @@
 #define MACROS_H
 
 #ifdef RAYTRACER_SLOW
-#define Assert(expression) { if(!(expression)) { *(int *)0 = 0; } }
+#define AssertBreak *(int *)0 = 0;
+#define Assert(expression) { if(!(expression)) { AssertBreak; } }
 #define FloatEqual(x, y) { return (abs(x - y) < FLT_EPSILON) ? true : false; }
+#define AssertZero(expression) { if((expression)) { AssertBreak } };
 
 #else
 
+#define AssertBreak;
 #define Assert(expression);
 #define FloatEqual(x, y);
+#define AssertZero(expression);
 #endif //RAYTRACER_SLOW
 
 #endif //MACROS_H
--- main.cpp
+++ main.cpp
@@ -1,19 +1,15 @@
 #include "macros.h"
+#include "math.h"
 
-typedef struct v4
+struct v4
 {
     float x, y, z, w;
-} point, vector;
-
-struct v3
-{
-    float x, y, z;
 };
 
 typedef float real32;
 
 inline v4
-set_vector(real32 x, real32 y, real32 z)
+vector(real32 x, real32 y, real32 z)
 { 
     v4 result; 
     result.x = x;
@@ -24,7 +20,7 @@ set_vector(real32 x, real32 y, real32 z)
 };
 
 inline v4
-set_point(real32 x, real32 y, real32 z)
+point(real32 x, real32 y, real32 z)
 {
     v4 result;
     result.x = x;
@@ -35,29 +31,96 @@ set_point(real32 x, real32 y, real32 z)
 }
 
 inline v4
-v4_add(v4 s, v4 t)
+v4_sub(v4 u, v4 v)
 {
     v4 result;
-    result.x = s.x + t.x;
-    result.y = s.y + t.y;
-    result.z = s.z + t.z;
-    result.w = s.w + t.w;
+    result.x = u.x - v.x;
+    result.y = u.y - v.y;
+    result.z = u.z - v.z;
+    result.w = u.w - v.w;
     return(result);
 }
 
 inline v4
-v4_sub(v4 s, v4 t)
+v4_add(v4 u, v4 v)
 {
     v4 result;
-    result.x = s.x - t.x;
-    result.y = s.y - t.y;
-    result.z = s.z - t.z;
-    result.w = s.w - t.w;
+    result.x = u.x + v.x;
+    result.y = u.y + v.y;
+    result.z = u.z + v.z;
+    result.w = u.w + v.w;
+    return(result);
+}
+
+inline v4
+v4_negate(v4 a)
+{
+    v4 result;
+    result.x = -a.x;
+    result.y = -a.y;
+    result.z = -a.z;
+    result.w = -a.w;
+    return(result);
+}
+
+inline v4
+v4_smul(v4 v, real32 a)
+{
+    v4 result;
+    result.x = v.x * a;
+    result.y = v.y * a;
+    result.z = v.z * a;
+    result.w = v.w * a;
+    return(result);
+}
+
+inline v4
+v4_sdiv(v4 v, real32 a)
+{
+    float i = 1 / a;
+    
+    v4 result;
+    result.x = v.x * i;
+    result.y = v.y * i;
+    result.z = v.z * i;
+    result.w = v.w * i;
+    return(result);
+}
+
+inline real32
+v4_dot(v4 u, v4 v)
+{
+    AssertZero(u.w);
+    AssertZero(v.w);
+    real32 result;
+    result = u.x * v.x + u.y * v.y + u.z * v.z + u.w * v.w;
+    return(result);
+}
+
+inline real32
+v4_magnitude(v4 v)
+{
+    AssertZero(v.w);
+    real32 result;
+    result = sqrt(v.x * v.x + v.y * v.y + v.z * v.z + v.w * v.w);
+    return(result);
+}
+
+inline v4
+v4_normalize(v4 v)
+{
+    AssertZero(v.w);
+    float inverse_magnitude = 1 / sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+    
+    v4 result;
+    result.x = v.x * inverse_magnitude;
+    result.y = v.y * inverse_magnitude;
+    result.z = v.z * inverse_magnitude;
+    result.w = 0;
     return(result);
 }
 
 int main()
 {
-    
     return(0);
 }
